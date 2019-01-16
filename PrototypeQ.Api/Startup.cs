39da4dc15c8164ccfb62222308d8e3ce1965ac98
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2019-01-16 23:23:23 +0200

    Add EF and DB context to project

--- PrototypeQ.Api/Startup.cs
+++ PrototypeQ.Api/Startup.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PrototypeQ.DataAccess.Contexts;
 
 namespace PrototypeQ.Api
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+		
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<ApplicationContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
 			services.AddCors();
 			services.AddMvc();
 		}
---
+++ PrototypeQ.DataAccess/Contexts/ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PrototypeQ.DataAccess.Contexts
+{
+	public class ApplicationContext : DbContext
+	{
+		public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+	}
+}
