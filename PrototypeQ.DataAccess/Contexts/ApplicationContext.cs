9d90e7fbdaa9e17dbe95f03246792477796aadab
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2019-01-16 23:33:51 +0200

    Add person entity

--- PrototypeQ.DataAccess/Contexts/ApplicationContext.cs
+++ PrototypeQ.DataAccess/Contexts/ApplicationContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PrototypeQ.DataAccess.Entities;
 
 namespace PrototypeQ.DataAccess.Contexts
 {
 	public class ApplicationContext : DbContext
 	{
 		public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
-
+		public DbSet<Person> People { get; set; }
 	}
 }
---
+++ PrototypeQ.DataAccess/Entities/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace PrototypeQ.DataAccess.Entities
+{
+	public class Person
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public int Age { get; set; }
+	}
+}
