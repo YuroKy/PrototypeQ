48891433ddae599bc3fbe5dd8e9b124b2c0303ee
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2019-01-29 22:34:09 +0200

    Add person service

---
+++ PrototypeQ.Services/Services/Abstractions/IPersonService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using PrototypeQ.DataModel.Models;
+
+namespace PrototypeQ.Services.Services.Abstractions
+{
+	public interface IPersonService
+	{
+		List<PersonModel> GetAll();
+	}
+}
---
+++ PrototypeQ.Services/Services/Implementations/PersonService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PrototypeQ.DataAccess.Contexts;
+using PrototypeQ.DataModel.Models;
+using PrototypeQ.Services.Mappers;
+using PrototypeQ.Services.Services.Abstractions;
+
+namespace PrototypeQ.Services.Services.Implementations
+{
+	public class PersonService : IPersonService
+	{
+		private readonly ApplicationContext _dbContext;
+
+		public PersonService(ApplicationContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public List<PersonModel> GetAll()
+		{
+			return _dbContext.People
+				.Select(PersonMapper.ToPersonModel)
+				.ToList();
+		}
+	}
+}
