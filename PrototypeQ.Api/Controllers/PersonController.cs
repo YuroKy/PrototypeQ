6ce78ee087fdbf4cfe072ea44dbbda94208c05d1
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2019-01-29 22:34:38 +0200

    Add person controller

---
+++ PrototypeQ.Api/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrototypeQ.Services.Services.Abstractions;
+
+namespace PrototypeQ.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PersonController : ControllerBase
+	{
+		private readonly IPersonService _personService;
+
+		public PersonController(IPersonService personService)
+		{
+			_personService = personService;
+		}
+
+		public IActionResult GetAll()
+		{
+			return Ok(_personService.GetAll());
+		}
+	}
+}
