f6045f6121180210f9b7dc9d49af9d9506534753
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2019-02-04 23:57:22 +0200

    Add field extension

--- PrototypeQ.Services/Abstractions/Services/IAliasService.cs
+++ PrototypeQ.Services/Abstractions/Services/IAliasService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using PrototypeQ.Data.Entities;
 
 namespace PrototypeQ.Services.Abstractions.Services
---
+++ PrototypeQ.Services/Implementations/Extensions/FieldExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace PrototypeQ.Services.Implementations.Extensions
+{
+	public static class FieldExtension
+	{
+		public static string GetFieldName(this FieldInfo fieldInfo)
+		{
+			return fieldInfo.Name.Split('>').First().Substring(1);
+		}
+	}
+}
